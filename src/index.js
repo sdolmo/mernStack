1c9ce025198055517f0a46976a2270de451f19d7
Sylvia <sylviad1221@gmail.com>
2017-02-27 02:26:11 -0500

    complete mern chat

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import CommentBox from './CommentBox';
 
 
 ReactDOM.render(
-  <CommentBox />,
+  <CommentBox
+  url='http://localhost:3001/api/comments'
+  pollInterval={2000} />,
   document.getElementById('root')
 );
